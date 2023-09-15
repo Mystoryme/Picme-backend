f0ad9064a1c37801494676d71a12a600ba695e0c
Panitnun-6243 <panitnun.6243@gmail.com>
2023-09-15 15:55:19 +0700

    debug: delete view template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pic me' });
+  res.send('Hello Pic me');
+});
+
+router.get("/posts", function (req, res, next) {
+  res.send("Post");
 });
 
 module.exports = router;
