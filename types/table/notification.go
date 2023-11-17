7981ee9c0dd7df85a3edc7b451446fe991636b32
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2023-11-17 16:38:40 +0700

    Update notification.go

--- types/table/notification.go
+++ types/table/notification.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Notification struct {
-	Id               *uint64            `gorm:"primaryKey"`
-	Trigger          *User              `gorm:"foreignKey:TriggerId"`
-	TriggerId        *uint64            `gorm:"not null"`
-	Triggee          *User              `gorm:"foreignKey:TriggeeId"`
-	TriggeeId        *uint64            `gorm:"not null"`
-	Post             *Post              `gorm:"foreignKey:PostId"`
-	PostId           *uint64            `gorm:"not null"`
+	Id               *uint64 `gorm:"primaryKey"`
+	Trigger          *User   `gorm:"foreignKey:TriggerId"`
+	TriggerId        *uint64
+	Triggee          *User `gorm:"foreignKey:TriggeeId"`
+	TriggeeId        *uint64
+	Post             *Post `gorm:"foreignKey:PostId"`
+	PostId           *uint64
 	NotificationType *enum.Notification `gorm:"type:ENUM('comment','like','user_donate','post_donate'); not null"`
 	CreatedAt        *time.Time         `gorm:"not null"` // Embedded field
 	UpdatedAt        *time.Time         `gorm:"not null"` // Embedded field
