ee10b9a12251178e65331020eb22530bb3e9800b
Mixko <apisitmaneerat@gmail.com>
2023-11-22 01:42:14 +0700

    feat: scb create qr

--- helper/scb.go
+++ helper/scb.go
@@ -10,7 +10,7 @@ import (
 	"picme-backend/types/payload"
 )
 
-func ScbGetAccessToken() string {
+func scbGetAccessToken() string {
 	url := mod.Conf.ScbUrl + "/v1/oauth/token"
 
 	getTokenBody := payload.ScbGetTokenRequest{
@@ -55,4 +55,58 @@ func ScbGetAccessToken() string {
 	}
 
 	return tokenResponse.Data.AccessToken
+}
+
+func ScbCreateQrPayment() {
+	accessToken := scbGetAccessToken()
+
+	url := mod.Conf.ScbUrl + "/payment/qrcode/create"
+
+	createQrBody := payload.ScbCreateQrPaymentRequest{
+		QrType: "PP",
+		Amount: "100.00",
+		PpType: "BILLERID",
+		PpId:   "1234567890",
+		Ref1:   "1234567890",
+		Ref2:   "1234567890",
+		Ref3:   "1234567890",
+	}
+
+	// Convert struct to JSON
+	jsonData, err := json.Marshal(createQrBody)
+	if err != nil {
+		logrus.Error("Unable to marshal JSON ", err)
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		logrus.Error("Unable to construct request ", err)
+		panic(err)
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("accept-language", "EN")
+	req.Header.Add("resourceOwnerId", mod.Conf.ScbAppKey)
+	req.Header.Add("requestUId", "1234567890")
+	req.Header.Add("authorization", "Bearer "+accessToken)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logrus.Error("Unable to send request ", err)
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		logrus.Error("Unable to read response body ", err)
+		panic(err)
+	}
+
+	var qrResponse *payload.ScbCreateQrResponse
+	if err := json.Unmarshal(body, &qrResponse); err != nil {
+		logrus.Error("Unable to parse response body ", err)
+		panic(err)
+	}
 }
--- types/payload/payment.go
+++ types/payload/payment.go
@@ -1,18 +1,24 @@
 package payload
 
+// ScbGetTokenRequest is the request body for getting access token
+
 type ScbGetTokenRequest struct {
 	ApplicationKey    string `json:"applicationKey"`
 	ApplicationSecret string `json:"applicationSecret"`
 }
 
-type ScbGetTokenResponse struct {
-	Status ScbGetTokenStatusResponse `json:"status,omitempty"`
-	Data   ScbGetTokenDataResponse   `json:"data,omitempty"`
-}
+// ScbStatusResponse is the response body for status
 
-type ScbGetTokenStatusResponse struct {
+type ScbStatusResponse struct {
 	Code        int    `json:"code,omitempty"`
 	Description string `json:"description,omitempty"`
+}
+
+// ScbGetTokenResponse is the response body for getting access token
+
+type ScbGetTokenResponse struct {
+	Status ScbStatusResponse       `json:"status,omitempty"`
+	Data   ScbGetTokenDataResponse `json:"data,omitempty"`
 }
 
 type ScbGetTokenDataResponse struct {
@@ -20,4 +26,26 @@ type ScbGetTokenDataResponse struct {
 	ExpiresIn   int    `json:"expiresIn,omitempty"`
 	TokenType   string `json:"tokenType,omitempty"`
 	ExpiresAt   int    `json:"expiresAt,omitempty"`
+}
+
+// ScbCreateQrPaymentRequest is the request body for creating QR payment
+
+type ScbCreateQrPaymentRequest struct {
+	QrType string `json:"qrType"`
+	Amount string `json:"amount"`
+	PpType string `json:"ppType"`
+	PpId   string `json:"ppId"`
+	Ref1   string `json:"ref1"`
+	Ref2   string `json:"ref2"`
+	Ref3   string `json:"ref3"`
+}
+
+type ScbCreateQrResponse struct {
+	Status ScbStatusResponse       `json:"status,omitempty"`
+	Data   ScbCreateQrDataResponse `json:"data,omitempty"`
+}
+
+type ScbCreateQrDataResponse struct {
+	QrRawData string `json:"qrRawData,omitempty"`
+	QrImage   string `json:"qrImage,omitempty"`
 }
