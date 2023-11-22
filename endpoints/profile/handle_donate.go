1eaa9c6a0e33f879110cc0253e85c349a852bd70
Mixko <apisitmaneerat@gmail.com>
2023-11-23 01:09:17 +0700

    feat: user donate

--- endpoints/profile/handle_donate.go
+++ endpoints/profile/handle_donate.go
@@ -1,8 +1,8 @@
 package profileEndpoint
 
 import (
+	"picme-backend/helper"
 	mod "picme-backend/modules"
-	"picme-backend/types/enum"
 	"picme-backend/types/misc"
 	"picme-backend/types/payload"
 	"picme-backend/types/response"
@@ -30,35 +30,47 @@ func DonateHandler(c *fiber.Ctx) error {
 
 	//create
 	//ctr+space เติมfillแบบรวดเร็ว
+	transactionId := text.GenerateTransactionId(10)
 
 	donate := &table.UserDonate{
-		Donor:        nil,
-		DonorId:      l.Id,
-		User:         nil,
-		UserId:       body.UserId,
-		DonateAmount: body.DonateAmount,
-		CreatedAt:    nil,
+		Donor:         nil,
+		DonorId:       l.Id,
+		Paid:          text.Ptr(false),
+		User:          nil,
+		UserId:        body.UserId,
+		DonateAmount:  body.DonateAmount,
+		TransactionId: &transactionId,
+		CreatedAt:     nil,
 	}
 
 	if result := mod.DB.Create(donate); result.Error != nil {
 		return response.Error(false, "Unable to donate artist", result.Error)
 	}
 
-	userDonateType := enum.NotificationUserDonate
-	notification := &table.Notification{
-		Trigger:          nil,
-		TriggerId:        l.Id,
-		Post:             nil,
-		PostId:           nil,
-		Triggee:          nil,
-		TriggeeId:        body.UserId,
-		NotificationType: &userDonateType,
-		CreatedAt:        nil,
+	// create qr code
+	qrData := helper.ScbCreateQrPayment(uint(*body.DonateAmount), transactionId)
+
+	donateResponse := payload.CreateDonateQrResponse{
+		TransactionId: transactionId,
+		QrImage:       qrData.QrImage,
+		QrRawData:     qrData.QrRawData,
 	}
 
-	if result := mod.DB.Create(&notification); result.Error != nil {
-		return response.Error(false, "Unable to create notification", result.Error)
-	}
+	//userDonateType := enum.NotificationUserDonate
+	//notification := &table.Notification{
+	//	Trigger:          nil,
+	//	TriggerId:        l.Id,
+	//	Post:             nil,
+	//	PostId:           nil,
+	//	Triggee:          nil,
+	//	TriggeeId:        body.UserId,
+	//	NotificationType: &userDonateType,
+	//	CreatedAt:        nil,
+	//}
+	//
+	//if result := mod.DB.Create(&notification); result.Error != nil {
+	//	return response.Error(false, "Unable to create notification", result.Error)
+	//}
 
-	return c.JSON(response.Info("Successfully donate!"))
+	return c.JSON(response.Info(donateResponse))
 }
--- types/table/user_donate.go
+++ types/table/user_donate.go
@@ -3,11 +3,13 @@ package table
 import "time"
 
 type UserDonate struct {
-	Donor        *User      `gorm:"foreignKey:DonorId"`
-	DonorId      *uint64    `gorm:"not null"` //คนที่donate
-	User         *User      `gorm:"foreignKey:UserId"`
-	UserId       *uint64    `gorm:"not null"` //postที่โดนdonate
-	DonateAmount *uint64    `gorm:"not null"`
-	CreatedAt    *time.Time `gorm:"not null"` // Embedded field
+	Donor         *User      `gorm:"foreignKey:DonorId"`
+	DonorId       *uint64    `gorm:"not null"` //คนที่donate
+	Paid          *bool      `gorm:"not null"`
+	User          *User      `gorm:"foreignKey:UserId"`
+	UserId        *uint64    `gorm:"not null"` //postที่โดนdonate
+	DonateAmount  *uint64    `gorm:"not null"`
+	TransactionId *string    `gorm:"not null"`
+	CreatedAt     *time.Time `gorm:"not null"` // Embedded field
 
 }
